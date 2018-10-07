a6574322160e35a94384e008bd8b61e2bf224338
zaidali08 <43931503+zaidali08@users.noreply.github.com>
2018-10-08 00:09:53 +0530

    Add files via upload

---
+++ frm.php
@@ -0,0 +1,141 @@
+<?php
+/**	$enno=$_POST["enno"];
+	$sname=$_POST["sname"];
+	$gen=$_POST["gen"];
+	$email=$_POST["email"];
+	$mob=$_POST["mob"];
+	
+	echo $enno;
+	echo $sname;
+	echo $gen;
+	echo $email;
+	echo $mob;
+*/
+?>
+<?php
+/**	$a=10;
+	$b=20;
+	
+	echo " A :- $a </br>";
+	echo " B :- $b </br>";
+	
+	echo "</br><h3> Arithmatic Operators </h3>";
+
+	$c=$a+$b;
+	echo "Addition : $c </br>";
+	$d=$a-$b;
+	echo "Substraction : $d </br>";
+	$e=$a*$b;
+	echo "Multiplication : $e </br>";
+	$f=$a/$b;
+	echo "Division : $f </br>";
+	$g=$a%$b;
+	echo "Modulus : $g ";
+
+	echo "</br><h3> Assignment Operators </h3>";
+
+	$a+=$b;
+	echo "Addition : $a </br>";
+	$a-=$b;
+	echo "Substraction : $a </br>";
+	$a*=$b;
+	echo "Multiplication : $a </br>";
+	$a/=$b;
+	echo "Division : $a </br>";
+	$a%=$b;
+	echo "Modulus : $a </br>";
+
+	echo "<h3> Comparison Operators </h3>";
+
+	$c=10;
+	$d="10";
+
+	if($c==$d)
+	{
+		echo " $c=$d '==' ";
+	}
+	else
+	{
+		echo " $c != $d '==' ";
+	}
+	
+	if($c===$d)
+	{
+		echo "</br> $c are same type $d '===' ";
+	}
+	else
+	{
+		echo "</br> $c are not same type $d '===' ";
+	}
+
+	if($c!=$d || $c<>$d)
+	{
+		echo "</br> $c != $d '!=' & '<>' ";
+	}
+	else
+	{
+		echo "</br> $c = $d '!=' & '<>' ";
+	}
+
+	if($c!==$d)
+	{
+		echo "</br> $c are not same type $d '!==' </br>";
+	}
+	else
+	{
+		echo "</br> $c are same type $d '!==' </br>";
+	}
+
+	echo "<h3> Logical Operators </h3>";
+
+	$e=20;
+	$f=10;
+
+	if($e==10 & $f==10)
+	{
+		echo " $e are same $f '&' </br>";
+	}
+	else
+	{
+		echo " $e are not same $f '&' </br>";
+	}
+	if($e==10 && $f==10)
+	{
+		echo " $e are same $f '&&' ";
+	}
+	else
+	{
+		echo " $e are not same $f '&&' ";
+	}
+	if($e==10 || $f==10)
+	{
+		echo "</br> $e are same $f '||' ";
+	}
+	else
+	{
+		echo "</br> $c are not same $d '||' ";
+	}
+
+	echo "<h3> Increment/Decrement Operators </h3>";
+
+	$e++;
+	$f++;
+
+	echo " e++ = $e & f++ = $f";
+
+	$e--;
+	$f--;
+
+	echo "</br> e -- = $e & f -- = $f";
+	
+	++$e;
+	++$f;
+
+	echo "</br> ++e = $e & ++f = $f";
+
+	--$e;
+	--$f;
+
+	echo "</br> -- e = $e &  -- f = $f";
+*/
+?>
